ac8645417153a0c5d5f47402dadfb3e3c52d487f
rolpe <ronlipkin14@gmail.com>
2016-01-30 14:03:30 -0500

    inital commit

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,16 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    @IBOutlet weak var multiplesLogo: UIImageView!
+    @IBOutlet weak var multTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var quitBtn: UIButton!
+    
+    var sum = 0
+    var prevSum = 0
+    var multiple = 0
+    var max = 1000
+    
+    @IBAction func onPlayBtnPress(sender: UIButton!) {
+        
+        if Int(multTxt.text!) != nil {
+            multiplesLogo.hidden = true
+            playBtn.hidden = true
+            multTxt.hidden = true
+            
+            quitBtn.hidden = false
+            resultLbl.hidden = false
+            addBtn.hidden = false
+            
+            multiple = Int(multTxt.text!)!
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onAddBtnPressed(sender: UIButton!) {
+        addMultiple()
+        updateResultLbl()
+        if sum >= 1000 {
+            resetGame()
+        }
     }
-
-
+    
+    @IBAction func onQuitBtnPressed(sender: UIButton!) {
+        resetGame()
+    }
+    
+    func addMultiple() {
+        prevSum = sum
+        sum = prevSum + multiple
+    }
+    
+    func updateResultLbl() {
+        resultLbl.text = "\(prevSum) + \(multiple) = \(sum)"
+    }
+    
+    func resetGame() {
+        multiplesLogo.hidden = false
+        playBtn.hidden = false
+        multTxt.hidden = false
+        
+        quitBtn.hidden = true
+        resultLbl.hidden = true
+        addBtn.hidden = true
+        
+        multiple = Int(multTxt.text!)!
+        sum = 0;
+        prevSum = 0;
+        resultLbl.text = "Press Add to add!"
+        multTxt.text = nil
+    }
+    
+    
 }
 
